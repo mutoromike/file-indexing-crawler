32b7029ab082ee01ef996cb88389b2149b292b33
strongestavenger <mikemutoro@gmail.com>
2019-03-07 15:39:37 +0300

    convert filenames to urls

--- filer.py
+++ filer.py
@@ -1,15 +1,14 @@
-import os
+import os, pathlib
 
 def all_files(directory):
     for path, dirs, files in os.walk(directory):
         for f in files:
-            # print(f)
             yield os.path.join(path, f)
-            # print(f)
 
-r3d_files = [f for f in all_files('/Users/strongestavenger/Desktop/music')
-               if f.endswith('.mp4')]
+video_files = [f for f in all_files('/Users/strongestavenger/Desktop/music')
+               if f.endswith(('.mp4', '.flv', '.avi', '.mov', '.wmv'))]
 
-# print(r3d_files)
-for file in r3d_files:
-    print(file)
+for file in video_files:
+    # Convert file-names to urls
+    file_url = pathlib.Path(file).as_uri()
+    print(url_file)
