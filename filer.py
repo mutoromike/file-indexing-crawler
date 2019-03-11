ac9ff7f4ef0b44625a6aa3df11b84f15267ecf69
strongestavenger <mikemutoro@gmail.com>
2019-03-11 20:58:54 +0300

    add method to get machine ip

--- filer.py
+++ filer.py
@@ -1,4 +1,13 @@
-import os, pathlib
+import os, pathlib, socket
+
+
+def get_machine_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    # print(s.getsockname()[0])
+    h_name = s.getsockname()[0]
+    s.close()
+    return h_name
 
 def all_files(directory):
     for path, dirs, files in os.walk(directory):
@@ -11,4 +20,7 @@ video_files = [f for f in all_files('/Users/strongestavenger/Desktop/music')
 for file in video_files:
     # Convert file-names to urls
     file_url = pathlib.Path(file).as_uri()
-    print(url_file)
+    
+    namer = get_machine_ip()
+    print('http://' + namer + '/' + file_url)
+    # print(namer)
